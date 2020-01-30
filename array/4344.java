b59550187d270079a3dd5e6fe841863569c106a6
CHAEWONSEO <60397314+chaewonseoo@users.noreply.github.com>
2020-01-30 16:37:51 +0900

    Update and rename 8958.java to 4344.java

--- array/4344.java
+++ array/4344.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class $8958 {
+public class $4344 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int c = sc.nextInt();
