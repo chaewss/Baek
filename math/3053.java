25d55981df74690a3903b7c1d474498b9eacb4b7
CHAEWONSEO <60397314+chaewonseoo@users.noreply.github.com>
2020-02-15 22:45:36 +0900

    Create 3053.java

---
+++ math/3053.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class $3053 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int r = in.nextInt();
+
+        System.out.printf("%.6f\n", r*r*Math.PI);
+        System.out.printf("%.6f", (double)(r*2)*r);
+    }
+}
