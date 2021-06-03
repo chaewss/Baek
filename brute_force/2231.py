0016c3f193d653e27c4a7f48d12b5539d06a8d11
서채원 <chaewseo12@gmail.com>
2021-06-04 01:14:07 +0900

    BOJ 2231

---
+++ brute_force/2231.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_li = []
+
+    for i in range(n):
+        weight, height = map(int, input().split())  # x = 몸무게, y = 키
+        student_li.append((weight, height))
+
+    for i in student_li:
+        rank = 1
+        for j in student_li:
+            if i[0] < j[0] and i[1] < j[1]:
+                rank += 1
+        print(rank, end=" ")
