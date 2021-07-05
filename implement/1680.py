e78656cd24a6e1458d7658d6e182710bbd8162dc
서채원 <chaewseo12@gmail.com>
2021-07-05 15:37:46 +0900

    BOJ

---
+++ implement/1680.py
@@ -0,0 +1,31 @@
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        w, n = map(int, input().split())
+        testcase = []
+        for __ in range(n):
+            x_i, w_i = map(int, input().split())
+            testcase.append([x_i, w_i])
+
+        capacity = 0
+        distance = 0
+        previous_value = 0
+        for i in testcase:
+            # i[0]: 쓰레기장으로부터의 거리   i[1]: 쓰레기의 양
+            if capacity + i[1] < w:
+                distance += i[0] - previous_value
+                capacity += i[1]
+            elif capacity + i[1] == w:
+                distance += (i[0] - previous_value) + i[0] * 2
+                capacity = 0
+                if testcase.index(i) == n - 1:
+                    distance -= i[0] * 2
+            elif capacity + i[1] > w:
+                capacity = i[1]
+                distance += (i[0] - previous_value) + i[0] * 2
+            previous_value = i[0]
+
+        # 모든 쓰레기를 수거하여 다시 쓰레기장으로 돌아갈 때
+        if testcase.index(i) == n - 1:
+            distance += i[0]
+
+        print(distance)
