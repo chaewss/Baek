6604d6901d37fe1709bac212c5eaf4f1d419b59f
서채원 <chaewseo12@gmail.com>
2021-07-11 17:22:37 +0900

    BOJ

---
+++ math/1252.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    a, b = input().split()
+    print(bin(int(a, 2) + int(b, 2))[2:])
---
+++ string/1652.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    n = int(input())
+    room = []
+    for _ in range(n):
+        room += [input() + 'X']
+    room.append('X' * (n+1))
+
+    row_seat, col_seat = 0, 0
+    for i in range(n + 1):
+        row_cnt, col_cnt = 0, 0
+        for j in range(n + 1):
+            # 가로
+            if room[i][j] == '.':
+                row_cnt += 1
+            elif room[i][j] == 'X':
+                if row_cnt >= 2:
+                    row_seat += 1
+                row_cnt = 0
+            # 세로
+            if room[j][i] == '.':
+                col_cnt += 1
+            elif room[j][i] == 'X':
+                if col_cnt >= 2:
+                    col_seat += 1
+                col_cnt = 0
+    print(row_seat, col_seat)
